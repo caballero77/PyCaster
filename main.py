2f587db00299c9fd9a721468425fdefb55457aad
romanvlasiuk90 <162982500+romanvlasiuk90@users.noreply.github.com>
2024-05-23 19:55:41 +0300

    потрібно під файл створювати або папку в папці програми або десь взагалі в корні
    тому в файл main.py
    
    вніс зміни у блок if __name__ == "__main__":
    
        #filename = Path(sys.argv[1] if len(sys.argv) > 1 else "address_book.pickle")
    
        # Встановлення шляху до файлу
        file_name = 'address_book.pickle'
        default_directory = Path.cwd() / 'my_address_book'
        #default_directory = Path.home() / 'my_address_book_dir2'
        filename = default_directory / file_name
    
        # Перевірка, чи файл знаходиться у зазначеній папці
        if not filename.exists():
            # Якщо ні, створюємо директорію
            filename.parent.mkdir(parents=True, exist_ok=True)
            # Оновлюємо шлях до файлу
            filename = filename.parent / file_name
    
    також в build_dependencies цього ж файлу main.py змінив filename.name на filename, щоб бачити повний файл звідки завантажує і куди пише

--- main.py
+++ main.py
@@ -18,12 +18,12 @@ def build_dependencies(filename: Path):
     if err:
         print(err)
         sys.exit(1)
-    print(f"Data has been loaded from file: {filename.name}")
+    print(f"Data has been loaded from file: {filename}")
     try:
         yield Dependencies(address_book)
     finally:
         address_book.save_data(filename)
-        print(f"Data has been saved to file: {filename.name}")
+        print(f"Data has been saved to file: {filename}")
             
 
 def main(filename: Path):
@@ -33,5 +33,20 @@ def main(filename: Path):
         
 
 if __name__ == "__main__":
-    filename = Path(sys.argv[1] if len(sys.argv) > 1 else "address_book.pickle")
+
+    #filename = Path(sys.argv[1] if len(sys.argv) > 1 else "address_book.pickle")
+    
+    # Встановлення шляху до файлу
+    file_name = 'address_book.pickle'
+    default_directory = Path.cwd() / 'my_address_book'
+    #default_directory = Path.home() / 'my_address_book_dir2'
+    filename = default_directory / file_name
+
+    # Перевірка, чи файл знаходиться у зазначеній папці
+    if not filename.exists():
+        # Якщо ні, створюємо директорію
+        filename.parent.mkdir(parents=True, exist_ok=True)
+        # Оновлюємо шлях до файлу
+        filename = filename.parent / file_name
+
     main(filename)
