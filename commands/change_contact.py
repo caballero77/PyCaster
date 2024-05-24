87c728c3d069a77bc0b0a8ddd24ff3b98d34e10b
Alex <alex.technic.2020@gmail.com>
2024-05-24 23:24:47 +0300

    fixing bugs with change and delete methods

--- commands/change_contact.py
+++ commands/change_contact.py
@@ -21,10 +21,11 @@ def change_contact(address_book: AddressBook) -> Command:
         
         Args:
             command (list[str]): The command to validate."""
-        if len(command) != 4:
-            raise MissingArgumentsError("name, field type, and new value.")
+        if not 3 <= len(command) < 5:  # modified to fit both cases: to edit values and changing phone number
+            raise MissingArgumentsError("must be [name], [field type], and [value]\n" + \
+                                        "or [old phone] and [new phone] values in case of changing existing phone number")
 
-        name, field_type, new_value = command[1:]
+        name, field_type, new_value = command[0], command[1], command[-1]
 
         if not address_book.has_record(name):
             raise InvalidArgumentsError(f"Contact you are trying to update not found: {name}")
@@ -42,6 +43,8 @@ def change_contact(address_book: AddressBook) -> Command:
         elif field_type == "birthday":
             if not Birthday.validate(new_value):
                 raise InvalidArgumentsError(f"Invalid birthday format: {new_value}")
+            # if {new_value} == not address_book.is_email_unique(new_value): 
+            #     raise InvalidArgumentsError(f"Email {new_value} already exists in the address book.") 
         elif field_type == "address":
             pass  # No validation for address
         else:
@@ -56,12 +59,16 @@ def change_contact(address_book: AddressBook) -> Command:
             command (list[str]): The command to execute.
             First element is the name of the contact, second element is the field type, third element is the new value.
         """
-        name, field_type, new_value = command[1:]
+        name, field_type, new_value = command[0], command[1], command[-1]
         record = address_book.find(name)
 
         match field_type:
             case "phone":
-                record.add_phone(new_value)
+                if len(command) == 3:  # will add new phone if 1 argument (new phone) in the 'change [name] phone' command
+                    record.add_phone(new_value)
+                elif len(command) == 4:  # will change to new phone if 2 arguments (old phone, new phone) in the command
+                    old_value = command[-2] 
+                    record.edit_phone(old_value, new_value) 
             case "email":
                 record.edit_email(new_value)
             case "address":
